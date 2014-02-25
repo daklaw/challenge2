c1bb0abd7c1a9a712ee668cf8b3355572270c5b4
David Law <daklaw@gmail.com>
2014-02-24 19:15:48 -0800

    Initial Commit

---
+++ challenge2Tests/challenge2Tests.m
@@ -0,0 +1,34 @@
+//
+//  challenge2Tests.m
+//  challenge2Tests
+//
+//  Created by David Law on 2/24/14.
+//  Copyright (c) 2014 David Law. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface challenge2Tests : XCTestCase
+
+@end
+
+@implementation challenge2Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
